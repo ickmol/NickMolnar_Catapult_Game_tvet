dc36f8c82573e717c86ddb890095fa1480121601
Ickmol <Ickmol@me.com>
2017-06-06 16:46:24 +1000

    added models

---
+++ TestProject1/Assets/Scripts/Bullet_Behaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_Behaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void FixedUpdate () {
+		transform.position += transform.forward;
+	}
+
+	private void OnCollisionEnter(Collision collidedObject) {
+		if (collidedObject.gameObject.tag == "Enemy")
+		{
+			print (collidedObject.gameObject.name);
+			print (collidedObject.transform.position);
+			Destroy (collidedObject.gameObject);
+		}
+	}
+}
---
+++ TestProject1/Assets/Scripts/ShootyScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootyScript : MonoBehaviour {
+
+	public bool isPressed;
+	private bool canShoot = true;
+	public float FireRate;
+	public GameObject bulletPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		isPressed = Input.GetKey (KeyCode.Mouse0);
+	}
+
+	void FixedUpdate() {
+		if (isPressed == true) {
+			if (canShoot) {
+				Instantiate (bulletPrefab, transform.position, transform.rotation);
+				canShoot = false;
+				StartCoroutine ("Wait");
+			}
+
+		}
+	}
+
+	private IEnumerator Wait() {
+		yield return new WaitForSeconds(FireRate);
+		canShoot = true;
+	}
+
+	private void OnCollisionEnter(Collision collidedObject){
+		if (GameObject.Find ("Enemy") == null) {
+			if (collidedObject.gameObject.tag == "Win") {
+			print ("You won!");
+			}
+		}
+	}
+}
